3573f3bb5024cebd9ae3867d2fdd29e70b19b8b2
Daniel <c2172268@live.tees.ac.uk>
2025-01-12 22:10:55 +0000

    initial commit

---
+++ ProfileService/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProfileService.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
