9c3c7b3144c9b3f8d15db6907bc65c079c907603
Daniel <c2172268@live.tees.ac.uk>
2025-01-13 00:18:31 +0000

    add authentication

--- ProfileService/Controllers/HomeController.cs
+++ ProfileService/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ProfileService.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 namespace ProfileService.Controllers;
+
 
 public class HomeController : Controller
 {
@@ -13,10 +18,44 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+   
     public IActionResult Index()
     {
         return View();
     }
+
+    public IActionResult Login()
+        {
+            try
+            {
+                return Challenge(new AuthenticationProperties { RedirectUri = "/" }, "Auth0");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error in Login: {ex.Message}");
+                return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+        }
+        // Logout logic
+        public async Task Logout()
+        {
+            try
+            {
+                await HttpContext.SignOutAsync("Auth0");
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties
+                {
+                    // Redirect to the home page after logout.
+                    RedirectUri = Url.Action("Index", "Home")
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error in Logout: {ex.Message}");
+            }
+
+            // Redirect to Home page or Login page after logout
+        }
 
     public IActionResult Privacy()
     {
