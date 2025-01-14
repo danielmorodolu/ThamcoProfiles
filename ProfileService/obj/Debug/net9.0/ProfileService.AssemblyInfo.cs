5d8851b58b49a5ec56688924a2ab0e47ee712c8e
Daniel <c2172268@live.tees.ac.uk>
2025-01-14 04:54:25 +0000

    update products

--- ProfileService/obj/Debug/net9.0/ProfileService.AssemblyInfo.cs
+++ ProfileService/obj/Debug/net9.0/ProfileService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProfileService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3335cf7a7cf429abced7f7c74a8b6c492b49b61f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af4da67393e2c80d17168c994ab7068f3a62b048")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProfileService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProfileService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
