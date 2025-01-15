b47f5923118ec2d7974929c3516f6ce4be90de5c
Daniel <c2172268@live.tees.ac.uk>
2025-01-15 13:30:59 +0000

    d

--- ProfileService/obj/Release/net9.0/ProfileService.AssemblyInfo.cs
+++ ProfileService/obj/Release/net9.0/ProfileService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProfileService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0968cb9ed735f875daf4f3cad3aa1cf0949e6d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1587bb2b23452e3d7bb64645b8badfa0b8e2d18")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProfileService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProfileService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
